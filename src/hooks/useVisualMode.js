f576582d9f46022e5450df80046384c22463a30f
Ralph Liton <rjayel@Ralphs-MacBook-Air.local>
2024-09-07 10:53:39 -0400

    Feat: Implementing Limits

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,11 +3,14 @@ import { useState } from 'react';
 export const useVisualMode = (initial) => {
   const [history, setHistory] = useState([initial]);
 
-  const transition = (newMode) => {
-    setHistory(prev => [...prev, newMode]);
+  const transition = (newMode, replace = false) => {
+    !replace && setHistory(prev => [...prev, newMode]);
+
+    replace && setHistory(prev => [...prev.slice(0, prev.length - 1), newMode])
   }
 
   const back = () => {
+    history.length > 1 &&
     setHistory(prev => [...prev.slice(0, prev.length - 1)])
 
   }
