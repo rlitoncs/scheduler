cc244961929993a3d5ee3295cf8cd1efe65ce1d0
Ralph Liton <rjayel@Ralphs-MacBook-Air.local>
2024-09-07 10:27:48 -0400

    Feat: adding back function

--- src/hooks/__tests__/useVisualMode.test.js
+++ src/hooks/__tests__/useVisualMode.test.js
@@ -4,6 +4,7 @@ import { useVisualMode } from "hooks/useVisualMode";
 
 const FIRST = "FIRST";
 const SECOND = "SECOND";
+const THIRD = "THIRD";
 
 test("useVisualMode should initialize with default value", () => {
   const { result } = renderHook(() => useVisualMode(FIRST));
@@ -11,4 +12,12 @@ test("useVisualMode should initialize with default value", () => {
   act(() => result.current.transition(SECOND));
   expect(result.current.mode).toBe(SECOND);
 
+  act(() => result.current.transition(THIRD));
+  expect(result.current.mode).toBe(THIRD);
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(SECOND);
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(FIRST);
 })
--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,13 +1,18 @@
 import { useState } from 'react';
 
 export const useVisualMode = (initial) => {
-  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
   const transition = (newMode) => {
-    setMode(newMode)
+    setHistory(prev => [...prev, newMode]);
   }
 
-  return { mode, transition }
+  const back = () => {
+    setHistory(prev => [...prev.slice(0, prev.length - 1)])
+
+  }
+
+  return { mode: history[history.length - 1], transition, back }
 };
 
 
