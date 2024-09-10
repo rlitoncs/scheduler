7807a24b66a9224cd83df8914af2c09cc8df8abf
Ralph Liton <rjayel@Ralphs-MacBook-Air.local>
2024-09-10 02:45:18 -0400

    Feat: Our First Test Complete

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,6 +6,7 @@ import Button from "components/Button";
 export default function Form(props) {
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
   function reset() {
     setName("");
@@ -16,6 +17,22 @@ export default function Form(props) {
     reset();
     props.onCancel();
   }
+
+  function validate() {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+
+    if (interviewer === null) {
+      setError("Please select an interviewer");
+      return;
+    }
+  
+  
+    props.onSave(name, interviewer);
+  }
+  
 
   return (
     <main className="appointment__card appointment__card--create">
@@ -30,8 +47,10 @@ export default function Form(props) {
             onChange={event => {
               setName(event.target.value);
             }}
+            data-testid="student-name-input"
           />
         </form>
+        <section className="appointment__validation">{error}</section>
         <InterviewerList
           interviewers={props.interviewers}
           value={interviewer}
@@ -43,7 +62,7 @@ export default function Form(props) {
           <Button danger onClick={cancel}>
             Cancel
           </Button>
-          <Button confirm onClick={() => {props.onSave(name, interviewer)}}>
+          <Button confirm onClick={validate}>
             Save
           </Button>
         </section>
